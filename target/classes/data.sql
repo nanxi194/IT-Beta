96aa0580827ee2bc5b4e4cb703741f9587f96120
Yiping LI <yipingl1@student.unimleb.edu.au>
2020-10-01 11:46:57 +1000

    infoEdit part

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(250) NOT NULL,
     last_name VARCHAR(250) NOT NULL,
     username VARCHAR(250) NOT NULL,
@@ -24,4 +24,19 @@ CREATE TABLE IF NOT EXISTS files(
     description TEXT,
     KEY project_id (project_id),
     CONSTRAINT `files_fk` FOREIGN KEY (project_id) REFERENCES projects (project_id)
+);
+
+CREATE TABLE IF NOT EXISTS user_info(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(250) NOT NULL,
+    last_name VARCHAR(250) NOT NULL,
+    contact_info VARCHAR(250) NOT NULL,
+    location VARCHAR(250) NOT NULL,
+    education TEXT NOT NULL,
+    work_exp TEXT NOT NULL,
+    summary TEXT NOT NULL,
+    picture BLOB,
+
+    KEY user_id (user_id),
+    CONSTRAINT `files_fk` FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
